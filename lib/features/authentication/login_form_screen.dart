1a936578f1d1449748c4975082f5b7ab906ffd13
Max16 <max16@naver.com>
2023-10-09 15:51:09 +0900

    5.1 Interests Screen

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -12,11 +15,38 @@ class LoginFormScreen extends StatefulWidget {
 
 class _LoginFormScreenState extends State<LoginFormScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
   Map<String, String> formData = {};
+  bool _obscureText = true;
+  String _email = "";
+  String _password = "";
 
   void _onScaffoldTap() {
     FocusScope.of(context).unfocus();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController.addListener(() {
+      setState(() {
+        _email = _emailController.text;
+      });
+    });
+    _passwordController.addListener(() {
+      setState(() {
+        _password = _passwordController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 
   void _onSubmitTap() {
@@ -26,6 +56,24 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
         print(formData);
       }
     }
+  }
+
+  void _onClearTap() {
+    _passwordController.clear();
+  }
+
+  void _toggleObscureText() {
+    _obscureText = !_obscureText;
+    setState(() {});
+  }
+
+  void _onSubmit() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const InterestsScreen(),
+      ),
+    );
   }
 
   @override
@@ -46,10 +94,31 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
               children: [
                 Gaps.v28,
                 TextFormField(
-                  decoration: const InputDecoration(
+                  inputFormatters: [
+                    FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                  ],
+                  controller: _emailController,
+                  decoration: InputDecoration(
                     hintText: 'E-mail',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
                   ),
+                  cursorColor: Theme.of(context).primaryColor,
                   validator: (value) {
+                    if (value == null) return null;
+                    final regExp = RegExp(
+                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+                    if (!regExp.hasMatch(value)) {
+                      return "E-mail is not valid";
+                    }
                     return null;
                   },
                   onSaved: (newValue) {
@@ -60,10 +129,58 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                 ),
                 Gaps.v16,
                 TextFormField(
-                  decoration: const InputDecoration(
+                  inputFormatters: [
+                    FilteringTextInputFormatter.deny(RegExp(r'\s')),
+                  ],
+                  obscureText: _obscureText,
+                  controller: _passwordController,
+                  onEditingComplete: () => _onSubmit(),
+                  decoration: InputDecoration(
+                    suffix: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        GestureDetector(
+                          onTap: _onClearTap,
+                          child: FaIcon(
+                            FontAwesomeIcons.circleXmark,
+                            color: Colors.grey.shade500,
+                            size: Sizes.size20,
+                          ),
+                        ),
+                        Gaps.h10,
+                        GestureDetector(
+                          onTap: _toggleObscureText,
+                          child: FaIcon(
+                            FontAwesomeIcons.eye,
+                            color: Colors.grey.shade500,
+                            size: Sizes.size20,
+                          ),
+                        ),
+                      ],
+                    ),
                     hintText: 'Password',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
                   ),
+                  cursorColor: Theme.of(context).primaryColor,
                   validator: (value) {
+                    if (value == null) return null;
+                    if (value.length <= 8) {
+                      return "Password is more than 8 Charecters";
+                    }
+                    final regExp = RegExp(
+                        r"^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@$!%*#?&\^])[A-Za-z0-9@$!%*#?&\^]{9,}$");
+                    if (!regExp.hasMatch(value)) {
+                      return 'Password must have Letters, numbers, and special characters';
+                    }
                     return null;
                   },
                   onSaved: (newValue) {
@@ -75,8 +192,8 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                 Gaps.v28,
                 GestureDetector(
                   onTap: _onSubmitTap,
-                  child: const FormButton(
-                    disabled: false,
+                  child: FormButton(
+                    disabled: _email.isEmpty || _password.length <= 8,
                     text: 'Log in',
                   ),
                 )
