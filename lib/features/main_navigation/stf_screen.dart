a427d14d0da209ab6e14b3a628684c5cd6fbd46c
Max16 <max16@naver.com>
2023-10-11 14:05:30 +0900

    6.7 Stateful navigation

---
+++ lib/features/main_navigation/stf_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class StfScreen extends StatefulWidget {
+  const StfScreen({super.key});
+
+  @override
+  State<StfScreen> createState() => _StfScreenState();
+}
+
+class _StfScreenState extends State<StfScreen> {
+  int _clicks = 0;
+
+  void _increase() {
+    setState(() {
+      _clicks = _clicks + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$_clicks',
+            style: const TextStyle(
+              fontSize: Sizes.size32,
+            ),
+          ),
+          TextButton(
+            onPressed: _increase,
+            child: const Text(
+              '+',
+              style: TextStyle(
+                fontSize: Sizes.size32,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
