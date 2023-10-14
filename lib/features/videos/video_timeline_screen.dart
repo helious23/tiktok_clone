fe0a8f70c8e831fd97dedcc070a5711ae8cd6e4b
Max16 <max16@naver.com>
2023-10-14 14:51:18 +0900

    7.3 Video Player

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_post.dart';
 
 class VideoTimelineScreen extends StatefulWidget {
   const VideoTimelineScreen({super.key});
@@ -8,32 +9,30 @@ class VideoTimelineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
-  final PageController _pageController = PageController();
   int _itemCount = 4;
 
-  List<Color> colors = [
-    Colors.blue,
-    Colors.red,
-    Colors.yellow,
-    Colors.teal,
-  ];
+  final PageController _pageController = PageController();
+
+  final Duration _scrollDuration = const Duration(milliseconds: 250);
+  final Curve _scrollCurve = Curves.linear;
 
   void _onPageChanged(int page) {
     _pageController.animateToPage(
       page,
-      duration: const Duration(milliseconds: 150),
-      curve: Curves.decelerate,
+      duration: _scrollDuration,
+      curve: _scrollCurve,
     );
     if (page == _itemCount - 1) {
       _itemCount = _itemCount + 4;
-      colors.addAll([
-        Colors.blue,
-        Colors.red,
-        Colors.yellow,
-        Colors.teal,
-      ]);
     }
     setState(() {});
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
   }
 
   @override
@@ -49,14 +48,8 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
       onPageChanged: (value) => _onPageChanged(value),
       itemCount: _itemCount,
       scrollDirection: Axis.vertical,
-      itemBuilder: (context, index) => Container(
-        color: colors.elementAt(index),
-        child: Center(
-          child: Text(
-            "Screen $index",
-            style: const TextStyle(fontSize: 68),
-          ),
-        ),
+      itemBuilder: (context, index) => VideoPost(
+        onVideoFinished: _onVideoFinished,
       ),
     );
   }
